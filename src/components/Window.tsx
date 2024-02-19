d6b2ff8c0fa811e52bb4aaaa60315ddb41e02b63
Luieitalian <enderbud125@hotmail.com>
2024-02-19 07:56:42 +0300

    testing styles

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -2,11 +2,9 @@ import React from "react"
 
 const Window = () => {
     return (
-        <div className="min-w-1">
-            <h1 className="bg-slate-900 text-sky-900">Hello World</h1>
+        <div className="w-5/6 drop-shadow-sm h-full bg-sky-700">
         </div>
     )
-
 }
 
 export default Window;

fa41d168c7ae5ed7c2d334b1dab4a798e053eb21
Luieitalian <enderbud125@hotmail.com>
2024-02-20 05:43:49 +0300

    add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import React from 'react'
+import React, { StrictMode } from 'react'
 import Window from "./components/Window"
 import "@/src/style.css"
 
 const container = document.getElementById('window');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<Window />);
+root.render(
+    <StrictMode>
+        <Window />
+    </StrictMode>
+);
